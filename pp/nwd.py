c5a6d2b8a5814590defa1e52e798bca87db17ace
Pawelonlinux <pawel.szymanski200q@gmail.com>
2025-03-10 16:17:42 +0100

    eukalides

--- pp/nwd.py
+++ pp/nwd.py
@@ -8,7 +8,7 @@ def nwd(a,b):
     return a
 
 
-print(nwd(120,21))
+#print(nwd(120,21))
 
 
 
@@ -22,10 +22,48 @@ def nwd_reku(a,b):
         return(nwd_reku(a,b-a))
 
 
-print(nwd_reku(120,21))
+#print(nwd_reku(120,21))
 
 
 # nwd_modulo_iteracyjne (reszta z dzielenia)
+def nwd_modulo_iteracyjnie(a,b):
+    while b!=0:
+        n = b 
+        b = a % b
+        a = n
+
+    return a
+
+
+def nwd_modulo_rekurencyjnie(a,b):
+    if b == 0:
+        return a
+    return nwd_modulo_rekurencyjnie(b, a % b)
+    
+        
+  
+   
+#print(nwd_modulo_rekurencyjnie(120,21))
+        
+
+
+
 # nwd_modulo_reku
 # i te wszystkie funkcje w petli jednej zrobic
 test_values = [ (315, 504), (1230, 528), (28, 8), (12, 18), (10000, 1) ]
+
+
+for i in range(len(test_values)):
+    a,b =test_values[i]
+    print("to a",a)
+    print("to b",b)
+
+    print("nwd iteracyjnie:",nwd(a,b))
+    print("nwd rekurencyjnie:",nwd(a,b))
+    print("nwd modulo iteracyjnie:",nwd(a,b))
+    print("nwd modulo rekurencyjnie:",nwd(a,b))
+
+
+
+
+
